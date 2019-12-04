d04b131d455c08d1ba43526e9001bc38f29a4108
Gia {hat <bomkillaz@gmail.com>
2019-12-04 23:21:17 +0700

    commit

--- app/src/main/java/com/example/swd1/models/remote/RetrofitConstants.java
+++ app/src/main/java/com/example/swd1/models/remote/RetrofitConstants.java
@@ -1,7 +1,7 @@
 package com.example.swd1.models.remote;
 
 public class RetrofitConstants {
-    public static final String BASE_URL = "http://192.168.10.113:4545/api/";
+    public static final String BASE_URL = "http://192.168.57.5:4545/api/";
 //    public static final String BASE_URL = "http://192.168.43.205:5555/api/";
 //    public static final String BASE_URL = "http://192.168.10.113:1470/api/";
 //    public static final String BASE_URL = "http://192.168.56.1:97/api/app/";
